b0290fb7322980548a590825642b7387c85b4494
ysdy44 <ysdy44@outlook.com>
2022-01-13 02:47:54 +0800

    Add PlayState

---
+++ Phagocytosis.Elements/PlayState.cs
@@ -0,0 +1,35 @@
+﻿namespace Phagocytosis.Elements
+{
+    /// <summary>
+    /// State of playing.
+    /// </summary>
+    public enum PlayState
+    {
+        /// <summary>
+        /// Playing
+        /// </summary>
+        Playing,
+        /// <summary>
+        /// Paused
+        /// </summary>
+        Paused,
+
+        /// <summary>
+        /// Losing
+        /// </summary>
+        Losing,
+        /// <summary>
+        /// Winning
+        /// </summary>
+        Winning,
+
+        /// <summary>
+        /// Loser
+        /// </summary>
+        Loser,
+        /// <summary>
+        /// Winner
+        /// </summary>
+        Winner,
+    }
+}

ca734a013818a1c81c8dbae75b9b652588525825
ysdy44 <ysdy44@outlook.com>
2022-01-13 23:04:40 +0800

    Add EditType

---
+++ Phagocytosis/Controls/EditType.cs
@@ -0,0 +1,27 @@
+﻿namespace Phagocytosis.Controls
+{
+    /// <summary>
+    /// Type of <see cref="EditCanvasControl"/>.
+    /// </summary>
+    public enum EditType
+    {
+        View,
+        Crop,
+        Clear,
+
+        Cursor,
+        AddCell,
+        AddBacteria,
+        AddVirus,
+        AddParamecium,
+        AddLeukocyte,
+        AddPrion,
+        AddCancer,
+
+        CursorRestricted,
+        AddRestricted,
+
+        ZoomIn,
+        ZoomOut,
+    }
+}

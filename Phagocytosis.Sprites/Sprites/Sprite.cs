fa0ecbb90bd28dc5ceeff32338296182fbfeafd7
ysdy44 <ysdy44@outlook.com>
2022-01-13 22:40:09 +0800

    Add Sprite

---
+++ Phagocytosis.Sprites/Sprites/Sprite.cs
@@ -0,0 +1,13 @@
+﻿namespace Phagocytosis.Sprites
+{
+    /// <summary>
+    /// Data of <see cref="Spriter"/>.
+    /// </summary>
+    public struct Sprite : ISprite
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public int Level { get; set; }
+        public int Type { get; set; }
+    }
+}

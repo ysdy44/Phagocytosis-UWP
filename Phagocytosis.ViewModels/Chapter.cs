e7651c5ba099eb41e552089d8e63e31eb3b344f9
ysdy44 <ysdy44@outlook.com>
2022-01-13 22:44:55 +0800

    Add Chapter

---
+++ Phagocytosis.ViewModels/Chapter.cs
@@ -0,0 +1,19 @@
+﻿using Phagocytosis.Sprites;
+using System.Collections.Generic;
+
+namespace Phagocytosis.ViewModels
+{
+    /// <summary>
+    /// Chapter
+    /// </summary>
+    public class Chapter
+    {
+        public int Width { set; get; }
+        public int Height { set; get; }
+        public int MaximumFoods { set; get; }
+        public int IncreaseFoods { set; get; }
+        public IEnumerable<Rect2> Restricteds { set; get; }
+        public IEnumerable<ISprite> FriendSprites { set; get; }
+        public IEnumerable<ISprite> EnemySprites { set; get; }
+    }
+}

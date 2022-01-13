9266f7fd093729af7026f70b360c88c84ed67b24
ysdy44 <ysdy44@outlook.com>
2022-01-13 23:21:44 +0800

    Add RecordEventArgs

---
+++ Phagocytosis/Controls/RecordEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Phagocytosis.Controls
+{
+    /// <summary>
+    /// Provides data about the <see cref="MainCanvasControl.Record"/> event.
+    /// </summary>
+    public class RecordEventArgs : EventArgs
+    {
+        public int FriendSpritesMaxLevel { get; set; }
+        public int FriendSpritesCount { get; set; }
+        public TimeSpan TotalTime { get; set; }
+    }
+}

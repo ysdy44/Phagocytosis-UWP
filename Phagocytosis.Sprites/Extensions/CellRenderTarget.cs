cecbdc66c422e26bb533b642a6d847c253f1ebdc
ysdy44 <ysdy44@outlook.com>
2022-01-13 22:38:09 +0800

    Add CellRenderTarget

---
+++ Phagocytosis.Sprites/Extensions/CellRenderTarget.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Graphics.Canvas;
+
+namespace Phagocytosis.Sprites
+{
+    /// <summary>
+    /// Cell for <see cref = "CanvasRenderTarget" />.
+    /// </summary>
+    public class CellRenderTarget
+    {
+
+        /// <summary>
+        /// Nuvleus for <see cref = "CanvasRenderTarget" />.
+        /// </summary>
+        public CanvasRenderTarget Nuvleus;
+
+        /// <summary>
+        /// Cytoplasm for <see cref = "CanvasRenderTarget" />.
+        /// </summary>
+        public CanvasRenderTarget Cytoplasm;
+
+    }
+}

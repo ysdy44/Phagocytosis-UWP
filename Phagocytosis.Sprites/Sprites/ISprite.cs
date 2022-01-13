5dc7a33aff16c710efa7216d6c5787a14590b9c1
ysdy44 <ysdy44@outlook.com>
2022-01-13 22:39:55 +0800

    Add ISprite

---
+++ Phagocytosis.Sprites/Sprites/ISprite.cs
@@ -0,0 +1,13 @@
+﻿namespace Phagocytosis.Sprites
+{
+    /// <summary>
+    /// Interface of <see cref="Spriter"/>.
+    /// </summary>
+    public interface ISprite
+    {
+        float X { get; }
+        float Y { get; }
+        int Level { get; }
+        int Type { get; }
+    }
+}

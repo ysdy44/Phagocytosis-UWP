429bd37b51630a92ee2f4cba742362fe27d1dfdd
ysdy44 <ysdy44@outlook.com>
2022-01-13 22:38:28 +0800

    Add CanvasResourceCreatorExtensions

---
+++ Phagocytosis.Sprites/Extensions/CanvasResourceCreatorExtensions.Brush.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Brushes;
+using Windows.UI;
+
+namespace Phagocytosis.Sprites
+{
+    /// <summary>
+    /// Extensions of <see cref = "ICanvasResourceCreator" />.
+    /// </summary>
+    public static partial class CanvasResourceCreatorExtensions
+    {
+
+        private static readonly CanvasGradientStop[] CellNuvleus = new CanvasGradientStop[]
+        {
+            new CanvasGradientStop
+            {
+                Position = 0,
+                Color = Color.FromArgb(58, 241, 175, 190),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.2f,
+                Color = Color.FromArgb(88, 210, 175, 190),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.5f,
+                Color = Color.FromArgb(122, 80, 171, 171),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.8f,
+                Color = Color.FromArgb(156, 1, 210, 244),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.95f,
+                Color = Color.FromArgb(255, 189, 219, 232),
+            },
+            new CanvasGradientStop
+            {
+                Position = 1,
+                Color = Colors.White
+            },
+        };
+        private static readonly CanvasGradientStop[] CellCytoplasm = new CanvasGradientStop[]
+        {
+            new CanvasGradientStop
+            {
+                Position = 0.2f,
+                Color = Color.FromArgb(255, 199, 231, 148),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.65f,
+                Color = Color.FromArgb(255, 93, 231, 189),
+            },
+            new CanvasGradientStop
+            {
+                Position = 1,
+                Color = Color.FromArgb(255, 167, 231, 207),
+            },
+        };
+
+        private static readonly CanvasGradientStop[] BacteriaNuvleus = new CanvasGradientStop[]
+        {
+            new CanvasGradientStop
+            {
+                Position = 0,
+                Color = Color.FromArgb(58, 255, 175, 255),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.2f,
+                Color = Color.FromArgb(88, 255, 175, 255),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.5f,
+                Color = Color.FromArgb(122, 255, 134, 255),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.8f,
+                Color = Color.FromArgb(156, 255, 122, 255),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.95f,
+                Color = Color.FromArgb(255, 255, 219, 255),
+            },
+            new CanvasGradientStop
+            {
+                Position = 1,
+                Color = Colors.White
+            },
+        };
+        private static readonly CanvasGradientStop[] BacteriaCytoplasm = new CanvasGradientStop[]
+        {
+            new CanvasGradientStop
+            {
+                Position = 0.2f,
+                Color = Color.FromArgb(255, 231, 146, 168),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.65f,
+                Color = Color.FromArgb(255, 231, 93, 189),
+            },
+            new CanvasGradientStop
+            {
+                Position = 1,
+                Color = Color.FromArgb(255, 231, 167, 207),
+            },
+        };
+
+        private static readonly CanvasGradientStop[] ParameciumCytoplasm = new CanvasGradientStop[]
+        {
+            new CanvasGradientStop
+            {
+                Position = 0,
+                Color = Color.FromArgb(255, 8, 48, 11),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.22f,
+                Color = Color.FromArgb(255, 12, 62, 11),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.39f,
+                Color = Color.FromArgb(255, 24, 76, 13),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.71f,
+                Color = Color.FromArgb(255, 69, 128, 20),
+            },
+            new CanvasGradientStop
+            {
+                Position = 1,
+                Color = Color.FromArgb(255, 124, 168, 45),
+            },
+        };
+
+        private static readonly CanvasGradientStop[] PrionCytoplasm = new CanvasGradientStop[]
+        {
+            new CanvasGradientStop
+            {
+                Position = 0,
+                Color = Color.FromArgb(209, 255, 31, 31),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.25f,
+                Color = Color.FromArgb(0, 254, 254, 246),
+            },
+            new CanvasGradientStop
+            {
+                Position = 0.81f,
+                Color = Color.FromArgb(84, 251, 255, 255),
+            },
+            new CanvasGradientStop
+            {
+                Position = 1,
+                Color = Color.FromArgb(189, 255, 255, 255),
+             },
+        };
+
+        private static readonly CanvasGradientStop[] StaphylococcusAureusNuvleus = new CanvasGradientStop[]
+        {
+             new CanvasGradientStop
+             {
+                Position = 0,
+                Color = Color.FromArgb(255 , 242, 211, 0),
+             },
+             new CanvasGradientStop
+             {
+                 Position = 0.3f,
+                 Color = Color.FromArgb(255 , 242, 211, 0),
+             },
+             new CanvasGradientStop
+             {
+                 Position = 0.35f,
+                 Color = Color.FromArgb(222, 237, 207, 0),
+             },
+             new CanvasGradientStop
+             {
+                 Position = 0.80f,
+                     Color = Color.FromArgb(186 , 255, 231, 64),
+             },
+             new CanvasGradientStop
+             {
+                 Position = 0.85f,
+                 Color = Color.FromArgb(255 , 255, 231, 0),
+              },
+             new CanvasGradientStop
+             {
+                 Position = 1,
+                 Color = Color.FromArgb(255 , 255, 255, 255),
+              },
+        };
+
+    }
+}
---
+++ Phagocytosis.Sprites/Extensions/CanvasResourceCreatorExtensions.cs
@@ -0,0 +1,348 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Brushes;
+using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas.Geometry;
+using System;
+using System.Numerics;
+using Windows.UI;
+
+namespace Phagocytosis.Sprites
+{
+    /// <summary>
+    /// Extensions of <see cref = "ICanvasResourceCreator" />.
+    /// </summary>
+    public static partial class CanvasResourceCreatorExtensions
+    {
+        
+        public static CellRenderTarget RenderCell(this ICanvasResourceCreatorWithDpi resourceCreator, int level, float radius, float diameter, Vector2 origin)
+        {
+            float radius2 = 2 * (float)Math.Pow(level, 0.333333333333333333333333333333333f);
+
+            CanvasRenderTarget nuvleus = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = nuvleus.CreateDrawingSession())
+            {
+                ds.FillCircle(origin, radius, new CanvasRadialGradientBrush(resourceCreator, CanvasResourceCreatorExtensions.CellNuvleus)
+                {
+                    Center = origin,
+                    RadiusX = radius,
+                    RadiusY = radius,
+                });
+                ds.DrawCircle(origin, radius, Colors.White, 2);
+            }
+
+            CanvasRenderTarget cytoplasm = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = cytoplasm.CreateDrawingSession())
+            {
+                ds.FillCircle(origin, radius2, new CanvasRadialGradientBrush(resourceCreator, CanvasResourceCreatorExtensions.CellCytoplasm)
+                {
+                    Center = origin,
+                    RadiusX = radius2,
+                    RadiusY = radius2,
+                });
+                ds.DrawCircle(origin, radius2, Colors.White, 2);
+            }
+
+            return new CellRenderTarget
+            {
+                Nuvleus = nuvleus,
+                Cytoplasm = cytoplasm
+            };
+        }
+
+
+        public static CellRenderTarget RenderBacteria(this ICanvasResourceCreatorWithDpi resourceCreator, int level, float radius, float diameter, Vector2 origin)
+        {
+            float radius2 = 2 * (float)Math.Pow(level, 0.333333333333333333333333333333333f);
+
+            CanvasRenderTarget nuvleus = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = nuvleus.CreateDrawingSession())
+            {
+                ds.FillCircle(origin, radius, new CanvasRadialGradientBrush(resourceCreator, CanvasResourceCreatorExtensions.BacteriaNuvleus)
+                {
+                    Center = origin,
+                    RadiusX = radius,
+                    RadiusY = radius,
+                });
+                ds.DrawCircle(origin, radius, Colors.White, 2);
+            }
+
+            CanvasRenderTarget cytoplasm = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = cytoplasm.CreateDrawingSession())
+            {
+                ds.FillCircle(origin, radius2, new CanvasRadialGradientBrush(resourceCreator, CanvasResourceCreatorExtensions.BacteriaCytoplasm)
+                {
+                    Center = origin,
+                    RadiusX = radius2,
+                    RadiusY = radius2,
+                });
+                ds.DrawCircle(origin, radius2, Colors.White, 2);
+            }
+
+            return new CellRenderTarget
+            {
+                Nuvleus = nuvleus,
+                Cytoplasm = cytoplasm
+            };
+        }
+
+
+        public static CellRenderTarget RenderVirus(this ICanvasResourceCreatorWithDpi resourceCreator, int level, float radius, float diameter, Vector2 origin)
+        {
+            float radius2 = 2 * (float)Math.Pow(level, 0.433333333333333333333333333333333f);
+
+            CanvasRenderTarget nuvleus = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = nuvleus.CreateDrawingSession())
+            {
+                ds.FillCircle(origin, radius2, new CanvasRadialGradientBrush(resourceCreator, Colors.Black, Colors.Red)
+                {
+                    Center = origin,
+                    RadiusX = radius2,
+                    RadiusY = radius2,
+                });
+            }
+
+            CanvasRenderTarget cytoplasm = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = cytoplasm.CreateDrawingSession())
+            {
+                ds.FillCircle(origin, radius - 2, Color.FromArgb(150, 0, 0, 0));
+                ds.DrawCircle(origin, radius - 2, Colors.Black, 2);
+                ds.DrawCircle(origin, radius2, Color.FromArgb(150, 255, 0, 0), 2);
+            }
+
+            return new CellRenderTarget
+            {
+                Nuvleus = nuvleus,
+                Cytoplasm = cytoplasm
+            };
+        }
+
+
+        public static CellRenderTarget RenderParamecium(this ICanvasResourceCreatorWithDpi resourceCreator, int level, float radius, float diameter, Vector2 origin)
+        {
+            float radius2 = 2 * (float)Math.Pow(level, 0.4633333333333333333333333333333333f);
+            float offset = radius - radius2;
+
+            float radius3 = radius2 - offset;
+
+            float strokeWidth = offset / 4 + 6;
+            float strokeWidth2 = offset / 12;
+
+            CanvasRenderTarget nuvleus = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = nuvleus.CreateDrawingSession())
+            {
+                ds.FillCircle(origin, radius, Color.FromArgb(54, 196, 230, 143));
+                ds.DrawCircle(origin, radius2, Color.FromArgb(188, 196, 230, 143), strokeWidth);
+                ds.DrawCircle(origin, radius2, Colors.White, strokeWidth2, new CanvasStrokeStyle
+                {
+                    DashStyle = CanvasDashStyle.Dash
+                });
+            }
+
+            CanvasRenderTarget cytoplasm = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = cytoplasm.CreateDrawingSession())
+            {
+                ds.FillCircle(origin, radius3, new CanvasRadialGradientBrush(resourceCreator, CanvasResourceCreatorExtensions.ParameciumCytoplasm)
+                {
+                    Center = origin,
+                    RadiusX = radius3,
+                    RadiusY = radius3,
+                });
+            }
+
+            return new CellRenderTarget
+            {
+                Nuvleus = nuvleus,
+                Cytoplasm = cytoplasm
+            };
+        }
+
+
+        public static CellRenderTarget RenderLeukocyte(this ICanvasResourceCreatorWithDpi resourceCreator, int level, float radius, float diameter, Vector2 origin)
+        {
+            float radius2 = 2 * (float)Math.Pow(level, 0.333333333333333333333333333333333f);
+
+            CanvasRenderTarget nuvleus = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = nuvleus.CreateDrawingSession())
+            {
+                ds.FillCircle(origin, radius, new CanvasRadialGradientBrush(resourceCreator, CanvasResourceCreatorExtensions.CellNuvleus)
+                {
+                    Center = origin,
+                    RadiusX = radius,
+                    RadiusY = radius,
+                });
+
+                ds.DrawCircle(origin, radius, Color.FromArgb(222, 255, 255, 255), 2);
+            }
+
+            CanvasRenderTarget cytoplasm = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = cytoplasm.CreateDrawingSession())
+            using (CanvasGeometry geometry = CanvasGeometry.CreateCircle(resourceCreator, origin, radius))
+            using (ds.CreateLayer(1, geometry))
+            {
+                ds.DrawImage(new InvertEffect
+                {
+                    Source = CanvasResourceCreatorExtensions.SpiderWebEffect(radius2, diameter)
+                });
+            }
+
+            return new CellRenderTarget
+            {
+                Nuvleus = nuvleus,
+                Cytoplasm = cytoplasm
+            };
+        }
+
+
+        public static CellRenderTarget RenderPrion(this ICanvasResourceCreatorWithDpi resourceCreator, int level, float radius, float diameter, Vector2 origin)
+        {
+            float radius2 = 2 * (float)Math.Pow(level, 0.333333333333333333333333333333333f);
+
+            CanvasRenderTarget nuvleus = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = nuvleus.CreateDrawingSession())
+            using (CanvasGeometry geometry = CanvasGeometry.CreateCircle(resourceCreator, origin, radius))
+            using (ds.CreateLayer(1, geometry))
+            {
+                ds.DrawImage(new OpacityEffect
+                {
+                    Opacity = 0.2f,
+                    Source = new InvertEffect
+                    {
+                        Source = CanvasResourceCreatorExtensions.SpiderWebEffect2(radius2, diameter)
+                    }
+                });
+
+                ds.FillCircle(origin, radius, new CanvasRadialGradientBrush(resourceCreator, CanvasResourceCreatorExtensions.PrionCytoplasm)
+                {
+                    Center = origin,
+                    RadiusX = radius,
+                    RadiusY = radius,
+                });
+
+                ds.DrawCircle(origin, radius, Colors.White, 2);
+            }
+
+            CanvasRenderTarget cytoplasm = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = cytoplasm.CreateDrawingSession())
+            {
+                ds.FillCircle(origin, radius2, new CanvasRadialGradientBrush(resourceCreator, Color.FromArgb(0, 0, 0, 255), Color.FromArgb(255, 100, 220, 255))
+                {
+                    Center = origin,
+                    RadiusX = radius2,
+                    RadiusY = radius2,
+                });
+
+                ds.DrawCircle(origin, radius2, Colors.White, 2);
+            }
+
+            return new CellRenderTarget
+            {
+                Nuvleus = nuvleus,
+                Cytoplasm = cytoplasm
+            };
+        }
+
+
+        public static CellRenderTarget RenderCancer(this ICanvasResourceCreatorWithDpi resourceCreator, int level, float radius, float diameter, Vector2 origin)
+        {
+            float radius2 = 2 * (float)Math.Pow(level, 0.433333333333333333333333333333333f);
+
+            CanvasRenderTarget nuvleus = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = nuvleus.CreateDrawingSession())
+            {
+                ds.FillCircle(origin, radius - 2, Color.FromArgb(150, 0, 0, 0));
+                ds.DrawCircle(origin, radius - 2, Colors.Black, 2);
+            }
+
+            CanvasRenderTarget cytoplasm = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = cytoplasm.CreateDrawingSession())
+            {
+                ds.DrawCircle(origin, radius2, Color.FromArgb(150, 255, 0, 0), 2);
+            }
+
+            return new CellRenderTarget
+            {
+                Nuvleus = nuvleus,
+                Cytoplasm = cytoplasm
+            };
+        }
+
+
+        public static CellRenderTarget RenderStaphylococcusAureus(this ICanvasResourceCreatorWithDpi resourceCreator, int level, float radius, float diameter, Vector2 origin)
+        {
+            float radius2 = 2 * (float)Math.Pow(level, 0.333333333333333333333333333333333f);
+
+            CanvasRenderTarget nuvleus = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = nuvleus.CreateDrawingSession())
+            {
+                ds.FillCircle(origin, radius, new CanvasRadialGradientBrush(resourceCreator, CanvasResourceCreatorExtensions.StaphylococcusAureusNuvleus)
+                {
+                    Center = origin,
+                    RadiusX = radius,
+                    RadiusY = radius,
+                });
+
+                ds.DrawCircle(origin, radius, Color.FromArgb(222, 255, 255, 255), 2);
+            }
+
+            CanvasRenderTarget cytoplasm = new CanvasRenderTarget(resourceCreator, diameter, diameter);
+            using (CanvasDrawingSession ds = cytoplasm.CreateDrawingSession())
+            using (CanvasGeometry geometry = CanvasGeometry.CreateCircle(resourceCreator, origin, radius))
+            using (ds.CreateLayer(1, geometry))
+            {
+                ds.DrawImage(new OpacityEffect
+                {
+                    Opacity = 0.8f,
+                    Source = new InvertEffect
+                    {
+                        Source = CanvasResourceCreatorExtensions.SpiderWebEffect(radius2, diameter)
+                    }
+                });
+            }
+
+            return new CellRenderTarget
+            {
+                Nuvleus = nuvleus,
+                Cytoplasm = cytoplasm
+            };
+        }
+
+
+        private static ICanvasImage SpiderWebEffect(float radius2, float diameter)
+        {
+            return new LuminanceToAlphaEffect
+            {
+                Source = new EdgeDetectionEffect
+                {
+                    Amount = 1,
+                    Source = new DiscreteTransferEffect
+                    {
+                        Source = new TurbulenceEffect
+                        {
+                            Octaves = 4, // 4
+                            Frequency = new Vector2(1f / radius2), // 0.02
+                            Size = new Vector2(diameter) // 222
+                        }
+                    }
+                }
+            };
+        }
+        private static ICanvasImage SpiderWebEffect2(float radius2, float diameter)
+        {
+            return new EdgeDetectionEffect
+            {
+                Amount = 1,
+                Source = new DiscreteTransferEffect
+                {
+                    Source = new TurbulenceEffect
+                    {
+                        Octaves = 4, // 4
+                        Frequency = new Vector2(1f / radius2), // 0.02
+                        Size = new Vector2(diameter) // 222
+                    }
+                }
+            };
+        }
+
+
+    }
+}

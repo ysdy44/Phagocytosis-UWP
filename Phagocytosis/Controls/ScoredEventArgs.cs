357e3ddcd8643bfca74a117b16387ebb854cdcce
ysdy44 <ysdy44@outlook.com>
2022-01-13 23:21:02 +0800

    Add ScoredEventArgs

---
+++ Phagocytosis/Controls/ScoredEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Phagocytosis.Controls
+{
+    /// <summary>
+    /// Provides data about the <see cref="MainCanvasControl.Scored"/> event.
+    /// </summary>
+    public class ScoredEventArgs : EventArgs
+    {
+        public int FriendSpritesSumLevel { get; set; }
+        public int EnemySpritesSumLevel { get; set; }
+    }
+}

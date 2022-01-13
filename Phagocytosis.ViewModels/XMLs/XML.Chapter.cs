670e1ef1f9c90818545d96e4bbfc073c4e1b50b2
ysdy44 <ysdy44@outlook.com>
2022-01-13 22:45:22 +0800

    Add XML

---
+++ Phagocytosis.ViewModels/XMLs/XML.Chapter.cs
@@ -0,0 +1,103 @@
+﻿using Phagocytosis.Sprites;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Phagocytosis.ViewModels
+{
+    public static partial class XML
+    {
+
+        public static XElement SaveChapter(string elementName, Chapter chapter)
+        {
+            XElement element = new XElement(elementName);
+
+            // SaveWith
+            {
+                element.Add(new XAttribute("Width", chapter.Width));
+                element.Add(new XAttribute("Height", chapter.Height));
+                element.Add(new XAttribute("MaximumFoods", chapter.MaximumFoods));
+                element.Add(new XAttribute("IncreaseFoods", chapter.IncreaseFoods));
+                if (chapter.Restricteds != null && chapter.Restricteds.Count() != 0)
+                {
+                    element.Add(new XElement
+                    (
+                        "Restricteds",
+                        from rect
+                        in chapter.Restricteds
+                        select new XElement("Rect", new XAttribute("X", rect.X), new XAttribute("Y", rect.Y), new XAttribute("Width", rect.Width), new XAttribute("Height", rect.Height))
+                    ));
+                }
+                if (chapter.FriendSprites != null && chapter.FriendSprites.Count() != 0)
+                {
+                    element.Add(new XElement
+                    (
+                        "FriendSprites",
+                        from friendSprite
+                        in chapter.FriendSprites
+                        select Sprites.XML.SaveSprite("Sprite", friendSprite)
+                    ));
+                }
+                if (chapter.EnemySprites != null && chapter.EnemySprites.Count() != 0)
+                {
+                    element.Add(new XElement
+                    (
+                        "EnemySprites",
+                        from enemySprite
+                        in chapter.EnemySprites
+                        select Sprites.XML.SaveSprite("Sprite", enemySprite)
+                    ));
+                }
+            }
+            return element;
+        }
+
+        public static Chapter LoadChapter(XElement element)
+        {
+            Chapter project = new Chapter();
+            if (element.Attribute("Width") is XAttribute width) project.Width = (int)width;
+            if (element.Attribute("Height") is XAttribute height) project.Height = (int)height;
+            if (element.Attribute("MaximumFoods") is XAttribute maximumFoods) project.MaximumFoods = (int)maximumFoods;
+            if (element.Attribute("IncreaseFoods") is XAttribute increaseFoods) project.IncreaseFoods = (int)increaseFoods;
+            if (element.Element("Restricteds") is XElement restricteds)
+            {
+                if (restricteds.Elements() is IEnumerable<XElement> restricteds2)
+                {
+                    project.Restricteds =
+                        from restricted
+                        in restricteds2
+                        select new Rect2
+                        (
+                            x: (int)restricted.Attribute("X"),
+                            y: (int)restricted.Attribute("Y"),
+                            width: (int)restricted.Attribute("Width"),
+                            height: (int)restricted.Attribute("Height")
+                        );
+                }
+            }
+            if (element.Element("FriendSprites") is XElement friendSprites)
+            {
+                if (friendSprites.Elements() is IEnumerable<XElement> friendSprite2)
+                {
+                    project.FriendSprites =
+                        from friendSprite
+                        in friendSprite2
+                        select Sprites.XML.LoadSprite(friendSprite);
+                }
+            }
+            if (element.Element("EnemySprites") is XElement enemySprites)
+            {
+                if (enemySprites.Elements() is IEnumerable<XElement> enemySprites2)
+                {
+                    project.EnemySprites =
+                        from enemySprite
+                        in enemySprites2
+                        select Sprites.XML.LoadSprite(enemySprite);
+                }
+            }
+
+            return project;
+        }
+
+    }
+}
---
+++ Phagocytosis.ViewModels/XMLs/XML.Chapters.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Phagocytosis.ViewModels
+{
+    public static partial class XML
+    {
+
+        public static XDocument SaveChapters(IEnumerable<Chapter> chapters)
+        {
+            return new XDocument
+            (
+                // Set the document definition for xml.
+                new XDeclaration("1.0", "utf-8", "no"),
+                new XElement
+                (
+                    "Root",
+                    from chapter
+                    in chapters
+                    select XML.SaveChapter("Chapter", chapter)
+                )
+            );
+        }
+
+        public static IEnumerable<Chapter> LoadChapters(XDocument document)
+        {
+            if (document.Element("Root") is XElement root)
+            {
+                if (root.Elements("Chapter") is IEnumerable<XElement> chapters)
+                {
+                    return
+                        from chapter
+                        in chapters
+                        select XML.LoadChapter(chapter);
+                }
+            }
+
+            return null;
+        }
+
+    }
+}

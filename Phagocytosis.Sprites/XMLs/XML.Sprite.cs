b0afd8be0e0bab8bbc1e094992e240220e6ee48a
ysdy44 <ysdy44@outlook.com>
2022-01-13 22:42:48 +0800

    Add XML

---
+++ Phagocytosis.Sprites/XMLs/XML.Sprite.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Linq;
+
+namespace Phagocytosis.Sprites
+{
+    public static partial class XML
+    {
+
+        public static XElement SaveSprite(string elementName, ISprite sprite)
+        {
+            XElement element = new XElement(elementName);
+
+            // SaveWith
+            {
+                element.Add(new XAttribute("X", sprite.X));
+                element.Add(new XAttribute("Y", sprite.Y));
+                element.Add(new XAttribute("Level", sprite.Level));
+                element.Add(new XAttribute("Type", sprite.Type));
+            }
+            return element;
+        }
+
+        public static ISprite LoadSprite(XElement element)
+        {
+            Sprite sprite = new Sprite();
+            if (element.Attribute("X") is XAttribute width) sprite.X = (float)width;
+            if (element.Attribute("Y") is XAttribute height) sprite.Y = (float)height;
+            if (element.Attribute("Level") is XAttribute level) sprite.Level = (int)level;
+            if (element.Attribute("Type") is XAttribute type) sprite.Type = (int)type;
+
+            return sprite;
+        }
+
+    }
+}

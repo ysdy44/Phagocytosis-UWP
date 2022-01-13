5a828a6c8d8c357c3ada8b4a546b46e0c55ac6ab
ysdy44 <ysdy44@outlook.com>
2022-01-13 22:39:14 +0800

    Add SpriteType

---
+++ Phagocytosis.Sprites/Sprites/SpriteType.cs
@@ -0,0 +1,18 @@
+﻿namespace Phagocytosis.Sprites
+{
+    /// <summary>
+    /// Type of <see cref="Spriter"/>.
+    /// </summary>
+    public enum SpriteType
+    {
+        Player,
+        Cell,
+        Bacteria,
+        Virus,
+        Paramecium,
+        Leukocyte,
+        Prion,
+        Cancer,
+        StaphylococcusAureus,
+    }
+}

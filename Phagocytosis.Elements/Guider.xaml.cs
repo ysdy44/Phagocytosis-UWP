6da929f9e05878c3111db4e38cc9e235e7291dd5
ysdy44 <ysdy44@outlook.com>
2022-01-16 01:26:28 +0800

    Add Guider

---
+++ Phagocytosis.Elements/Guider.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Phagocytosis.Elements
+{
+    /// <summary>
+    /// Represents a guider.
+    /// </summary>
+    public sealed partial class Guider : UserControl
+    {
+
+        //@Construct
+        /// <summary>
+        /// Initializes a Guider. 
+        /// </summary>
+        public Guider()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Begin the story-board.
+        /// </summary>
+        public void Begin()
+        {
+            this.Storyboard.Begin();
+        }
+
+    }
+}

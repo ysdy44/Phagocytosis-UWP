91d86e07fee18a81d9c5220349114fe4b1ed995f
ysdy44 <ysdy44@outlook.com>
2022-01-16 01:53:52 +0800

    Add CanvasStopwatch

---
+++ Phagocytosis.Elements/CanvasStopwatch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Phagocytosis.Elements
+{
+    /// <summary>
+    /// Stopwatch of <see cref="CanvasAnimatedControl"/>.
+    /// </summary>
+    public class CanvasStopwatch
+    {
+        TimeSpan Duration;
+        TimeSpan Starting;
+        TimeSpan Now;
+        public TimeSpan TotalTime() => this.Now - this.Starting + this.Duration;
+        public void Update(TimeSpan now) => this.Now = now;
+        public void Restart() => this.Duration = TimeSpan.Zero;
+        public void Play() => this.Starting = this.Now;
+        public void Pause() => this.Duration += this.Now - this.Starting;
+    }
+}
